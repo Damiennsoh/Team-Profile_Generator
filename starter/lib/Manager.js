97095cbb013e1f7bfeaf137d3fe76da12b5eaf65
Damiennsoh <psycaro20@gmail.com>
2023-02-01 20:57:39 +0530

    installed and added dependencies

---
+++ starter/lib/Manager.js
@@ -0,0 +1,17 @@
+// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+class Manager extends Employee {
+    constructor (name, id, email,);
+
+}
+
+getRole() {
+    return "Manager";
+
+}
+officeNumber(officeNumber) {
+    this.officeNumber = officeNumber;
+}
+
+getofficeNumber() {
+    return this.officeNumber;
+}
